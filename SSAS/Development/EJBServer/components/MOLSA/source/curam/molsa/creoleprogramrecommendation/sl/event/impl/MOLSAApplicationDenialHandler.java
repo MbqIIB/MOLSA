6c65896bc2112c94f26a70ca81303f3dd3240d5d
Diona_Molsa <Diona_Molsa@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-01-29 19:15:38 +0000

    Added more data to Milestone delivery creation - Smitha Changed the code on the start date of the milestone.

--- SSAS/Development/EJBServer/components/MOLSA/source/curam/molsa/creoleprogramrecommendation/sl/event/impl/MOLSAApplicationDenialHandler.java
+++ SSAS/Development/EJBServer/components/MOLSA/source/curam/molsa/creoleprogramrecommendation/sl/event/impl/MOLSAApplicationDenialHandler.java
@@ -10,6 +10,12 @@ import curam.application.entity.struct.ApplicationKey;
 import curam.application.impl.Application;
 import curam.application.impl.ApplicationDAO;
 import curam.application.impl.ProgramApplication;
+import curam.codetable.impl.MILESTONESTATUSCODEEntry;
+import curam.core.sl.entity.struct.MilestoneDeliveryKey;
+import curam.core.sl.fact.MilestoneDeliveryFactory;
+import curam.core.sl.intf.MilestoneDelivery;
+import curam.core.sl.struct.MilestoneDeliveryDtls;
+import curam.core.sl.struct.ReadMilestoneDeliveryDetails;
 import curam.creoleprogramrecommendation.codetable.impl.SIMULATEDDETERMINATIONDECLINEDREASONEntry;
 import curam.creoleprogramrecommendation.facade.fact.CREOLEProgramRecommendationFactory;
 import curam.creoleprogramrecommendation.facade.intf.CREOLEProgramRecommendation;
@@ -25,6 +31,7 @@ import curam.util.events.struct.Event;
 import curam.util.exception.AppException;
 import curam.util.exception.InformationalException;
 import curam.util.persistence.GuiceWrapper;
+import curam.util.type.Date;
 
 public class MOLSAApplicationDenialHandler implements EventHandler {
 
@@ -51,58 +58,95 @@ public class MOLSAApplicationDenialHandler implements EventHandler {
 	public void eventRaised(Event event) throws AppException,
 			InformationalException {
 
-		CaseHeader caseHeader = caseHeaderDAO.get(event.secondaryEventData);
-		ApplicationKey key = new ApplicationKey();
-		boolean actionTaken = false;
+		MilestoneDelivery deliveryObj = MilestoneDeliveryFactory.newInstance();
 
-		final List<Application> applications = applicationDAO
-				.searchByCase(caseHeader);
-		for (Iterator<Application> iterator = applications.iterator(); iterator
-				.hasNext();) {
-			Application application = iterator.next();
-			key.applicationID = application.getID();
-			if (!APPLICATIONSTATUSEntry.DISPOSED.getCode().equals(
-					application.getStatus())) {
-				CREOLEProgramRecommendation creoleProgramRecommendationObj = CREOLEProgramRecommendationFactory
-						.newInstance();
-				SimulatedDeterminationDetailsList simulatedDeterminationDtlsList = creoleProgramRecommendationObj
-						.listLatestAppliedForEligibleSimulatedDeterminations(key);
-				for (Iterator<SimulatedDeterminationDetails> iterator1 = simulatedDeterminationDtlsList.list
-						.iterator(); iterator1.hasNext();) {
-					SimulatedDeterminationDetails details = iterator1.next();
+		MilestoneDeliveryKey deliveryKey = new MilestoneDeliveryKey();
+		deliveryKey.milestoneDeliveryID = event.primaryEventData;
+		ReadMilestoneDeliveryDetails readMilestoneDeliveryDetails = deliveryObj
+				.read(deliveryKey);
+		if (!readMilestoneDeliveryDetails.readDetails.status
+				.equals(MILESTONESTATUSCODEEntry.COMPLETED.getCode())) {
 
-					if (details.isActionPending) {
-						final curam.creoleprogramrecommendation.impl.CREOLEProgramRecommendation creoleProgramRecommendation = creoleProgramRecommendationDAO
-								.get(details.creoleProgramRecommendationID);
-						final SimulatedDetermination simulatedDetermination = creoleProgramRecommendation
-								.getSimulatedDetermination(details.simulatedDeterminationID);
-						simulatedDeterminationManager
-								.decline(
-										creoleProgramRecommendation,
-										simulatedDetermination,
-										SIMULATEDDETERMINATIONDECLINEDREASONEntry.APPLICATION_DENIED,
-										"");
-					} else {
-						actionTaken = true;
+			CaseHeader caseHeader = caseHeaderDAO.get(event.secondaryEventData);
+			ApplicationKey key = new ApplicationKey();
+			boolean actionTaken = false;
+
+			final List<Application> applications = applicationDAO
+					.searchByCase(caseHeader);
+			for (Iterator<Application> iterator = applications.iterator(); iterator
+					.hasNext();) {
+				Application application = iterator.next();
+				key.applicationID = application.getID();
+				if (!APPLICATIONSTATUSEntry.DISPOSED.getCode().equals(
+						application.getStatus())) {
+					CREOLEProgramRecommendation creoleProgramRecommendationObj = CREOLEProgramRecommendationFactory
+							.newInstance();
+					SimulatedDeterminationDetailsList simulatedDeterminationDtlsList = creoleProgramRecommendationObj
+							.listLatestAppliedForEligibleSimulatedDeterminations(key);
+					for (Iterator<SimulatedDeterminationDetails> iterator1 = simulatedDeterminationDtlsList.list
+							.iterator(); iterator1.hasNext();) {
+						SimulatedDeterminationDetails details = iterator1
+								.next();
+
+						if (details.isActionPending) {
+							final curam.creoleprogramrecommendation.impl.CREOLEProgramRecommendation creoleProgramRecommendation = creoleProgramRecommendationDAO
+									.get(details.creoleProgramRecommendationID);
+							final SimulatedDetermination simulatedDetermination = creoleProgramRecommendation
+									.getSimulatedDetermination(details.simulatedDeterminationID);
+							simulatedDeterminationManager
+									.decline(
+											creoleProgramRecommendation,
+											simulatedDetermination,
+											SIMULATEDDETERMINATIONDECLINEDREASONEntry.APPLICATION_DENIED,
+											"");
+						} else {
+							actionTaken = true;
+						}
+
+					}
+					List<ProgramApplication> programApplications = application
+							.getProgramApplications();
+					for (Iterator<ProgramApplication> iterator2 = programApplications
+							.iterator(); iterator2.hasNext();) {
+						ProgramApplication programApplication = iterator2
+								.next();
+						if (actionTaken) {
+							programApplication.approve();
+						} else {
+							programApplication.deny();
+						}
+
 					}
 
 				}
-				List<ProgramApplication> programApplications = application
-						.getProgramApplications();
-				for (Iterator<ProgramApplication> iterator2 = programApplications
-						.iterator(); iterator2.hasNext();) {
-					ProgramApplication programApplication = iterator2.next();
-					if (actionTaken) {
-						programApplication.approve();
-					} else {
-						programApplication.deny();
-					}
-
-				}
-
 			}
+
+			completeMilestone(readMilestoneDeliveryDetails, event);
+
 		}
 
+	}
+
+	private void completeMilestone(
+			ReadMilestoneDeliveryDetails readMilestoneDeliveryDetails,
+			Event paramEvent) throws AppException, InformationalException {
+		MilestoneDelivery milestoneDeliveryObj = MilestoneDeliveryFactory
+				.newInstance();
+		MilestoneDeliveryDtls modifyMilestoneDeliveryDtls = new MilestoneDeliveryDtls();
+		modifyMilestoneDeliveryDtls.dtls.milestoneDeliveryID = paramEvent.primaryEventData;
+		modifyMilestoneDeliveryDtls.dtls.actualStartDate = readMilestoneDeliveryDetails.readDetails.actualStartDate;
+		modifyMilestoneDeliveryDtls.dtls.actualEndDate = Date.getCurrentDate();
+		modifyMilestoneDeliveryDtls.dtls.caseID = readMilestoneDeliveryDetails.readDetails.caseID;
+		modifyMilestoneDeliveryDtls.dtls.comments = readMilestoneDeliveryDetails.readDetails.comments;
+		modifyMilestoneDeliveryDtls.dtls.expectedEndDate = readMilestoneDeliveryDetails.readDetails.expectedEndDate;
+		modifyMilestoneDeliveryDtls.dtls.expectedStartDate = readMilestoneDeliveryDetails.readDetails.expectedStartDate;
+		modifyMilestoneDeliveryDtls.dtls.milestoneConfigurationID = readMilestoneDeliveryDetails.readDetails.milestoneConfigurationID;
+		modifyMilestoneDeliveryDtls.dtls.ownerUserName = readMilestoneDeliveryDetails.readDetails.ownerUserName;
+		modifyMilestoneDeliveryDtls.dtls.status = MILESTONESTATUSCODEEntry.COMPLETED
+				.toString();
+		modifyMilestoneDeliveryDtls.dtls.versionNo = readMilestoneDeliveryDetails.readDetails.versionNo;
+
+		milestoneDeliveryObj.modify(modifyMilestoneDeliveryDtls);
 	}
 
 }
