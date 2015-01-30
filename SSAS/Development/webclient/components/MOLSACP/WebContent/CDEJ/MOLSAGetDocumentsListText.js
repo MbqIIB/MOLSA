dcad03209a7e5a6c6a890940f74837b5007fe216
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-01-30 07:04:24 +0000

    Removed Alerts from JavaScript - Rajesh

--- SSAS/Development/webclient/components/MOLSACP/WebContent/CDEJ/MOLSAGetDocumentsListText.js
+++ SSAS/Development/webclient/components/MOLSACP/WebContent/CDEJ/MOLSAGetDocumentsListText.js
@@ -2,17 +2,11 @@ function getDocumentFormat() {
 
 	var cat = dijit.byId('__o3id0_0');
 	var template = dijit.byId('__o3id0');
-	
-	alert("Category :" +cat);
-	alert("Category Template:" +template);
-	
 	var category=String(cat);
 	var templateString=String(template);
 	if(category.length == 0 ||templateString.length == 0 ){
 		dojo.byId("__o3id1").innerHTML = "";
 	}else{
-	alert("Else Loop Template:" +category);
-	alert("Else Loop Template:" +templateString);
 		var xhrArgs = {
 				url : "MOLSA_getDocumentMessageTextPage.do",
 				
