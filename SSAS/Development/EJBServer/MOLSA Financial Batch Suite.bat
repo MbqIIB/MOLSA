bd4d40d6a0870140966b902afa17bb146c2ab2e0
gbm_molsa1 <gbm_molsa1@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-02-03 06:10:04 +0000

    Ditto Pachen- Added the Parameter

--- SSAS/Development/EJBServer/MOLSA Financial Batch Suite.bat
+++ SSAS/Development/EJBServer/MOLSA Financial Batch Suite.bat
@@ -1,4 +1,4 @@
-call build runbatch -Dbatch.program=curam.core.intf.DetermineProductDeliveryEligibility.process -Dbatch.username=SYSTEM
-call build runbatch -Dbatch.program=curam.core.intf.GenerateInstructionLineItems.processAllFinancialComponentsDue   -Dbatch.username=SYSTEM
-call build runbatch -Dbatch.program=curam.core.intf.GenerateInstruments.processInstructionLineItemsDue   -Dbatch.username=SYSTEM
-call build runbatch -Dbatch.program=curam.molsa.eft.batch.intf.MOLSAGenerateEFTBatch.process -Dbatch.username=SYSTEM
+call build runbatch -Dbatch.program=curam.core.intf.DetermineProductDeliveryEligibility.process -Dbatch.username=SYSTEM -Djava.maxmemory=1573m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
+call build runbatch -Dbatch.program=curam.core.intf.GenerateInstructionLineItems.processAllFinancialComponentsDue   -Dbatch.username=SYSTEM -Djava.maxmemory=1573m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
+call build runbatch -Dbatch.program=curam.core.intf.GenerateInstruments.processInstructionLineItemsDue   -Dbatch.username=SYSTEM -Djava.maxmemory=1573m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
+call build runbatch -Dbatch.program=curam.molsa.eft.batch.intf.MOLSAGenerateEFTBatch.process -Dbatch.username=SYSTEM -Djava.maxmemory=1573m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
