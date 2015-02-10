cff198827f9147170c0d1f99db619a15dc22b024
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-02-10 14:04:16 +0000

    Defect Fix 112-Rajesh

--- SSAS/Development/EJBServer/components/MOLSA/tests/curam/molsa/test/screening/rules/TestScreeningFamilyOfPrisonerProgram.java
+++ SSAS/Development/EJBServer/components/MOLSA/tests/curam/molsa/test/screening/rules/TestScreeningFamilyOfPrisonerProgram.java
@@ -1,64 +1,92 @@
 package curam.molsa.test.screening.rules;
 
+import java.util.List;
+
 import curam.creole.ruleclass.MOLSAScreeningRuleSet.impl.FamilyOfPrisonerProgram;
 import curam.creole.ruleclass.MOLSAScreeningRuleSet.impl.FamilyOfPrisonerProgram_Factory;
+import curam.creole.ruleclass.MOLSAScreeningRuleSet.impl.Person;
 import curam.molsa.codetable.impl.ABSENTFATHEREntry;
 import curam.molsa.test.base.MOLSAScreeningRulesTestData;
 
 public class TestScreeningFamilyOfPrisonerProgram extends
-    MOLSAScreeningRulesTestData {
+		MOLSAScreeningRulesTestData {
 
-  public TestScreeningFamilyOfPrisonerProgram(String name) {
-    super(name);
-  }
-  
-  public void testFamilyOfPrisonerBaseScenario() {
-	  createFamilyOfPrisonerBaseScenario(ABSENTFATHEREntry.INPRISON.getCode());
-	    FamilyOfPrisonerProgram familyOfPrisonerProgram = FamilyOfPrisonerProgram_Factory
-	        .getFactory().newInstance(getSession());
-	    assertEquals(Boolean.TRUE,
-	        familyOfPrisonerProgram.getAttributeValue(isEligible).getValue());
+	public TestScreeningFamilyOfPrisonerProgram(String name) {
+		super(name);
+	}
 
-	  }
+	public void testFamilyOfPrisonerBaseScenario() {
+		List<Person> personsList = createFamilyOfPrisonerBaseScenario(ABSENTFATHEREntry.INPRISON
+				.getCode());
+		FamilyOfPrisonerProgram familyOfPrisonerProgram = FamilyOfPrisonerProgram_Factory
+				.getFactory().newInstance(getSession());
+		assertEquals(Boolean.TRUE,
+				familyOfPrisonerProgram.getAttributeValue(isEligible)
+						.getValue());
+		String unitMembers = familyOfPrisonerProgram.getAttributeValue("name")
+				.getValue().toString();
+		assertEquals("Firdoz,Asifa,Fathima",
+				unitMembers.replaceAll(", ", ","));
 
+	}
+	
+	public void testFamilyOfPrisonerBaseScenarioVariationOne() {
+		createFamilyOfPrisonerBaseScenarioVariationOne(ABSENTFATHEREntry.INPRISON
+				.getCode());
+		FamilyOfPrisonerProgram familyOfPrisonerProgram = FamilyOfPrisonerProgram_Factory
+				.getFactory().newInstance(getSession());
+		assertEquals(Boolean.TRUE,
+				familyOfPrisonerProgram.getAttributeValue(isEligible)
+						.getValue());
+		String unitMembers = familyOfPrisonerProgram.getAttributeValue("name")
+				.getValue().toString();
+		assertEquals("Asifa",
+				unitMembers.replaceAll(", ", ","));
 
-  public void testFamilyOfPrisonerProgram() {
-    createFamilyOfPrisoner(ABSENTFATHEREntry.INPRISON.getCode());
-    FamilyOfPrisonerProgram familyOfPrisonerProgram = FamilyOfPrisonerProgram_Factory
-        .getFactory().newInstance(getSession());
-    assertEquals(Boolean.TRUE,
-        familyOfPrisonerProgram.getAttributeValue(isEligible).getValue());
+	}
+	
 
-  }
+	public void testFamilyOfPrisonerProgram() {
+		createFamilyOfPrisoner(ABSENTFATHEREntry.INPRISON.getCode());
+		FamilyOfPrisonerProgram familyOfPrisonerProgram = FamilyOfPrisonerProgram_Factory
+				.getFactory().newInstance(getSession());
+		assertEquals(Boolean.TRUE,
+				familyOfPrisonerProgram.getAttributeValue(isEligible)
+						.getValue());
 
-  public void testFamilyOfPrisonerProgramNegative() {
-    createFamilyOfPrisoner(ABSENTFATHEREntry.DECEASED.getCode());
+	}
 
-    FamilyOfPrisonerProgram familyOfPrisonerProgram = FamilyOfPrisonerProgram_Factory
-        .getFactory().newInstance(getSession());
-    assertEquals(Boolean.FALSE,
-        familyOfPrisonerProgram.getAttributeValue(isEligible).getValue());
+	public void testFamilyOfPrisonerProgramNegative() {
+		createFamilyOfPrisoner(ABSENTFATHEREntry.DECEASED.getCode());
 
-  }
+		FamilyOfPrisonerProgram familyOfPrisonerProgram = FamilyOfPrisonerProgram_Factory
+				.getFactory().newInstance(getSession());
+		assertEquals(Boolean.FALSE,
+				familyOfPrisonerProgram.getAttributeValue(isEligible)
+						.getValue());
 
-  public void testFamilyOfPrisonerProgramNegative1() {
-    createFamilyOfPrisoner(ABSENTFATHEREntry.MISSING.getCode());
+	}
 
-    FamilyOfPrisonerProgram familyOfPrisonerProgram = FamilyOfPrisonerProgram_Factory
-        .getFactory().newInstance(getSession());
-    assertEquals(Boolean.FALSE,
-        familyOfPrisonerProgram.getAttributeValue(isEligible).getValue());
+	public void testFamilyOfPrisonerProgramNegative1() {
+		createFamilyOfPrisoner(ABSENTFATHEREntry.MISSING.getCode());
 
-  }
+		FamilyOfPrisonerProgram familyOfPrisonerProgram = FamilyOfPrisonerProgram_Factory
+				.getFactory().newInstance(getSession());
+		assertEquals(Boolean.FALSE,
+				familyOfPrisonerProgram.getAttributeValue(isEligible)
+						.getValue());
 
-  public void testFamilyOfPrisonerProgramNegative2() {
-    createFamilyOfPrisoner(ABSENTFATHEREntry.NOT_SPECIFIED.getCode());
+	}
 
-    FamilyOfPrisonerProgram familyOfPrisonerProgram = FamilyOfPrisonerProgram_Factory
-        .getFactory().newInstance(getSession());
-    assertEquals(Boolean.FALSE,
-        familyOfPrisonerProgram.getAttributeValue(isEligible).getValue());
+	public void testFamilyOfPrisonerProgramNegative2() {
+		createFamilyOfPrisoner(ABSENTFATHEREntry.NOT_SPECIFIED.getCode());
 
-  }
+		FamilyOfPrisonerProgram familyOfPrisonerProgram = FamilyOfPrisonerProgram_Factory
+				.getFactory().newInstance(getSession());
+		assertEquals(Boolean.FALSE,
+				familyOfPrisonerProgram.getAttributeValue(isEligible)
+						.getValue());
+
+	}
 
 }
