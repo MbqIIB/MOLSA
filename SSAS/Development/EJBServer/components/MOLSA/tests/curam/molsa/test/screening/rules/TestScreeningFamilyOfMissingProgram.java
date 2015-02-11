db11fecb3bd97d5edf5afd88a341637c364ef39a
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-02-11 09:57:34 +0000

    Defect 100 - junit test cases

--- SSAS/Development/EJBServer/components/MOLSA/tests/curam/molsa/test/screening/rules/TestScreeningFamilyOfMissingProgram.java
+++ SSAS/Development/EJBServer/components/MOLSA/tests/curam/molsa/test/screening/rules/TestScreeningFamilyOfMissingProgram.java
@@ -6,28 +6,42 @@ import curam.molsa.codetable.impl.ABSENTFATHEREntry;
 import curam.molsa.test.base.MOLSAScreeningRulesTestData;
 
 public class TestScreeningFamilyOfMissingProgram extends
-    MOLSAScreeningRulesTestData {
+		MOLSAScreeningRulesTestData {
 
-  public TestScreeningFamilyOfMissingProgram(String name) {
-    super(name);
-  }
+	public TestScreeningFamilyOfMissingProgram(String name) {
+		super(name);
+	}
 
-  public void testFamilyOfMissingProgram() {
-    createFamilyOfMissingBaseScenario(ABSENTFATHEREntry.MISSING.getCode());
-    FamilyOfMissingProgram familyOfMissingProgram = FamilyOfMissingProgram_Factory
-        .getFactory().newInstance(getSession());
-    assertEquals(Boolean.TRUE,
-        familyOfMissingProgram.getAttributeValue(isEligible).getValue());
+	public void testFamilyOfMissingProgram() {
+		createFamilyOfMissingBaseScenario(ABSENTFATHEREntry.MISSING.getCode());
+		FamilyOfMissingProgram familyOfMissingProgram = FamilyOfMissingProgram_Factory
+				.getFactory().newInstance(getSession());
+		assertEquals(Boolean.TRUE,
+				familyOfMissingProgram.getAttributeValue(isEligible).getValue());
 
-  }
+	}
 
-  public void testFamilyOfMissingNegative() {
-    createFamilyOfMissingBaseScenario(ABSENTFATHEREntry.ANONYMOUS.getCode());
-    FamilyOfMissingProgram familyOfMissingProgram = FamilyOfMissingProgram_Factory
-        .getFactory().newInstance(getSession());
-    assertEquals(Boolean.FALSE,
-        familyOfMissingProgram.getAttributeValue(isEligible).getValue());
+	public void testFamilyOfMissingProgramVariationOne() {
+		createFamilyOfMissingBaseScenarioVariationOne(ABSENTFATHEREntry.MISSING
+				.getCode());
+		FamilyOfMissingProgram familyOfMissingProgram = FamilyOfMissingProgram_Factory
+				.getFactory().newInstance(getSession());
+		assertEquals(Boolean.TRUE,
+				familyOfMissingProgram.getAttributeValue(isEligible).getValue());
+		String unitMembers = familyOfMissingProgram.getAttributeValue("name")
+				.getValue().toString();
+		assertEquals("Asifa",
+				unitMembers.replaceAll(", ", ","));
 
-  }
+	}
+
+	public void testFamilyOfMissingNegative() {
+		createFamilyOfMissingBaseScenario(ABSENTFATHEREntry.ANONYMOUS.getCode());
+		FamilyOfMissingProgram familyOfMissingProgram = FamilyOfMissingProgram_Factory
+				.getFactory().newInstance(getSession());
+		assertEquals(Boolean.FALSE,
+				familyOfMissingProgram.getAttributeValue(isEligible).getValue());
+
+	}
 
 }
