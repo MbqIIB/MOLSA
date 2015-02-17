1f6505ae50a8fa4b440e87e4ed75bcbb515589d9
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-02-17 11:20:14 +0000

    Defect 209 -Rajesh

--- SSAS/Development/EJBServer/components/MOLSA/source/curam/rules/functions/CustomFunctionPersonInfo.java
+++ SSAS/Development/EJBServer/components/MOLSA/source/curam/rules/functions/CustomFunctionPersonInfo.java
@@ -17,80 +17,104 @@ import curam.util.rules.functor.Adaptor;
 import curam.util.rules.functor.AdaptorFactory;
 import curam.util.rules.functor.CustomFunctor;
 import curam.util.type.Date;
+
 /**
  * 
  * this is to give the person information
- *
+ * 
  */
 @SuppressWarnings("restriction")
 public class CustomFunctionPersonInfo extends CustomFunctor {
-  @Override
-  public Adaptor getAdaptorValue(RulesParameters rulesParameters)
-      throws AppException, InformationalException {
-    final IEG2Context ieg2Context = (IEG2Context) rulesParameters;
-    updateChildrenInfo(ieg2Context);
-    return AdaptorFactory.getBooleanAdaptor(Boolean.TRUE);
-  }
-/**
- * 
- * @param ieg2Context
- * an arguement storing ieg context value
- * @throws AppException
- * general exception
- * @throws InformationalException
- * general exception
- */
-  private void updateChildrenInfo(final IEG2Context ieg2Context)
-      throws AppException, InformationalException {
-    Datastore datastore = null;
-    try {
-      datastore = MOLSADatastoreUtility.getDatastoreInstance();
+	@Override
+	public Adaptor getAdaptorValue(RulesParameters rulesParameters)
+			throws AppException, InformationalException {
+		final IEG2Context ieg2Context = (IEG2Context) rulesParameters;
+		updateChildrenInfo(ieg2Context);
+		return AdaptorFactory.getBooleanAdaptor(Boolean.TRUE);
+	}
 
-      final Entity application = datastore.readEntity(ieg2Context
-          .getRootEntityID());
+	/**
+	 * 
+	 * @param ieg2Context
+	 *            an arguement storing ieg context value
+	 * @throws AppException
+	 *             general exception
+	 * @throws InformationalException
+	 *             general exception
+	 */
+	private void updateChildrenInfo(final IEG2Context ieg2Context)
+			throws AppException, InformationalException {
+		Datastore datastore = null;
+		try {
+			datastore = MOLSADatastoreUtility.getDatastoreInstance();
 
-      final Entity[] personEntities = application.getChildEntities(datastore
-          .getEntityType(MOLSADatastoreConst.kPerson));
+			final Entity application = datastore.readEntity(ieg2Context
+					.getRootEntityID());
 
-      for (Entity personEntity : personEntities) {
-        // Update Age
-        personEntity.setAttribute(MOLSADatastoreConst.kCalculatedAge,String.valueOf(MOLSADateUtil
-            .calculateAge(Date.getDate(personEntity
-                .getAttribute(MOLSADatastoreConst.kDateOfBirth)))));
+			final Entity[] personEntities = application
+					.getChildEntities(datastore
+							.getEntityType(MOLSADatastoreConst.kPerson));
 
-        if ((Integer.valueOf(personEntity
-            .getAttribute(MOLSADatastoreConst.kCalculatedAge)).intValue()) > 60) {
-          personEntity.setTypedAttribute(MOLSADatastoreConst.kIsSeniorCitizen,
-              Boolean.TRUE);
-        }
-        if (!(personEntity
-            .getTypedAttribute(MOLSADatastoreConst.kMaritalStatus)
-            .equals(MARITALSTATUSEntry.MARRIED.getCode()))) {
+			for (Entity personEntity : personEntities) {
+				// Update Age
+				personEntity
+						.setAttribute(
+								MOLSADatastoreConst.kCalculatedAge,
+								String.valueOf(MOLSADateUtil.calculateAge(Date.getDate(personEntity
+										.getAttribute(MOLSADatastoreConst.kDateOfBirth)))));
 
-          if ((personEntity.getTypedAttribute(MOLSADatastoreConst.kGender)
-              .equals(GENDEREntry.MALE.getCode()))
-              && (Boolean) (personEntity
-                  .getTypedAttribute(MOLSADatastoreConst.kIsMemberEnrolledInSchool))) {
-            personEntity.setTypedAttribute(MOLSADatastoreConst.kIsChild,
-                Boolean.TRUE);
-            personEntity.update();
-          } else if ((personEntity
-              .getTypedAttribute(MOLSADatastoreConst.kGender)
-              .equals(GENDEREntry.FEMALE.getCode()))) {
-            personEntity.setTypedAttribute(MOLSADatastoreConst.kIsChild,
-                Boolean.TRUE);
-            personEntity.update();
-          }
-        } else {
-          personEntity.setTypedAttribute(MOLSADatastoreConst.kIsChild,
-              Boolean.FALSE);
-          personEntity.update();
-        }
-      }
-      return;
+				if ((Integer.valueOf(personEntity
+						.getAttribute(MOLSADatastoreConst.kCalculatedAge))
+						.intValue()) > 60) {
+					personEntity.setTypedAttribute(
+							MOLSADatastoreConst.kIsSeniorCitizen, Boolean.TRUE);
+				}
+				if (!(personEntity
+						.getTypedAttribute(MOLSADatastoreConst.kMaritalStatus)
+						.equals(MARITALSTATUSEntry.MARRIED.getCode()))) {
 
-    } catch (NoSuchSchemaException e) {
-      throw new AppRuntimeException(e);
-    }
-  }
+					if ((personEntity
+							.getTypedAttribute(MOLSADatastoreConst.kGender)
+							.equals(GENDEREntry.MALE.getCode()))) {
+						if ((Boolean) (personEntity
+								.getTypedAttribute(MOLSADatastoreConst.kIsMemberEnrolledInSchool))) {
+							personEntity.setTypedAttribute(
+									MOLSADatastoreConst.kIsChild, Boolean.TRUE);
+						} else if (!(Boolean) (personEntity
+								.getTypedAttribute(MOLSADatastoreConst.kIsMemberEnrolledInSchool))) {
+							if (null == personEntity
+									.getTypedAttribute("educationLevel")) {
+								personEntity.setTypedAttribute(
+										MOLSADatastoreConst.kIsChild,
+										Boolean.TRUE);
+							}
+
+							else if ((Boolean) (personEntity
+									.getTypedAttribute("educationLevel")
+									.equals("MEL17010"))) {
+								personEntity.setTypedAttribute(
+										MOLSADatastoreConst.kIsChild,
+										Boolean.TRUE);
+							}
+						}
+						personEntity.update();
+					} else if ((personEntity
+							.getTypedAttribute(MOLSADatastoreConst.kGender)
+							.equals(GENDEREntry.FEMALE.getCode()))) {
+						personEntity.setTypedAttribute(
+								MOLSADatastoreConst.kIsChild, Boolean.TRUE);
+						personEntity.update();
+					}
+				} else {
+					personEntity.setTypedAttribute(
+							MOLSADatastoreConst.kIsChild, Boolean.FALSE);
+					personEntity.update();
+				}
+			}
+			return;
+
+		} catch (NoSuchSchemaException e) {
+			throw new AppRuntimeException(e);
+		}
+	}
 }
