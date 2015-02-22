98f94b2d16da9b5f12d646031d27fe362e9ede05
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-02-22 06:14:05 +0000

    Ditto Pachen - Batch Checkins

---
+++ SSAS/Development/EJBServer/MOLSA Bulk PDC Approval.bat
@@ -0,0 +1,1 @@
+call build runbatch -Dbatch.program=curam.molsa.pdc.generator.intf.MOLSABulkPDCApproveBatch.process -Dbatch.username=SYSTEM -Djava.maxmemory=1024m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
