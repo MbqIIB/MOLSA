775844f132f582a7228c7e3737d0f40064e95472
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-02-22 14:23:26 +0000

    Ditto Pachen - Removed a bat file

--- SSAS/Development/EJBServer/MOLSA Bulk PDC.bat
+++
@@ -1,1 +0,0 @@
-call build runbatch -Dbatch.program=curam.molsa.pdc.generator.intf.MOLSABulkPDCGeneratorBatch.process -Dbatch.username=SYSTEM -Djava.maxmemory=1024m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
