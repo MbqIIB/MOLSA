e39e2a5beadbc288f4eea630ae6f5b51c41a2de3
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-02-24 14:21:50 +0000

    Ditto Pachen - Batch Checkins

---
+++ SSAS/Development/EJBServer/MOLSA Financial Batch Suite For Scheduler.bat
@@ -0,0 +1,9 @@
+D:
+CD D:\Qatar\MolsaSVN\Development\
+call SetEnvironment.bat
+CD EJBServer
+
+call build runbatch -Dbatch.program=curam.core.intf.DetermineProductDeliveryEligibility.process -Dbatch.username=SYSTEM -Djava.maxmemory=1024m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
+call build runbatch -Dbatch.program=curam.core.intf.GenerateInstructionLineItems.processAllFinancialComponentsDue   -Dbatch.username=SYSTEM -Djava.maxmemory=1024m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
+call build runbatch -Dbatch.program=curam.core.intf.GenerateInstruments.processInstructionLineItemsDue   -Dbatch.username=SYSTEM -Djava.maxmemory=1024m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
+call build runbatch -Dbatch.program=curam.molsa.eft.batch.intf.MOLSAGenerateEFTBatch.process -Dbatch.username=SYSTEM -Djava.maxmemory=1024m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
