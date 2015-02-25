c44e8ce908e246057bd9a914c719a2d5ea35cdb9
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-02-25 14:04:51 +0000

    Ditto Pachen - Batch Checkins

---
+++ SSAS/Development/EJBServer/MOLSA Information Provider Batch.bat
@@ -0,0 +1,1 @@
+call build runbatch -Dbatch.program=curam.molsa.ip.batch.intf.MOLSAInformationProviderBatch.process -Dbatch.username=SYSTEM -Djava.maxmemory=1024m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
