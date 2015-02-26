9b5f51915a1324d33708fb0800b0b539e5f6dc5e
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-02-26 06:51:43 +0000

    Ditto Pachen- Financial Batch Scheduler Batch

--- SSAS/Development/EJBServer/MOLSA Financial Batch Suite For Scheduler.bat
+++ SSAS/Development/EJBServer/MOLSA Financial Batch Suite For Scheduler.bat
@@ -1,7 +1,6 @@
-D:
-CD D:\Qatar\MolsaSVN\Development\
+E:
+CD E:\IBM\Curam\Runtime
 call SetEnvironment.bat
-CD EJBServer
 
 call build runbatch -Dbatch.program=curam.core.intf.DetermineProductDeliveryEligibility.process -Dbatch.username=SYSTEM -Djava.maxmemory=1024m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
 call build runbatch -Dbatch.program=curam.core.intf.GenerateInstructionLineItems.processAllFinancialComponentsDue   -Dbatch.username=SYSTEM -Djava.maxmemory=1024m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
