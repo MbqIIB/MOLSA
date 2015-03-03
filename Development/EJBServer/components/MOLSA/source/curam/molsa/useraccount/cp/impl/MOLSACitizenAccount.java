14fce839844b152bb8616240efd271630b4be649
Diona_Molsa <Diona_Molsa@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-03-03 14:36:45 +0000

    Added javadoc and comments : Pavan

--- Development/EJBServer/components/MOLSA/source/curam/molsa/useraccount/cp/impl/MOLSACitizenAccount.java
+++ Development/EJBServer/components/MOLSA/source/curam/molsa/useraccount/cp/impl/MOLSACitizenAccount.java
@@ -7,12 +7,21 @@ import curam.util.exception.InformationalException;
 
 public class MOLSACitizenAccount extends
 		curam.molsa.useraccount.cp.base.MOLSACitizenAccount {
-
+	/**
+	 * This method is used to call the helper method which creates new account
+	 * for the applicant
+	 * 
+	 * @param key
+	 *            ConcernRoleKey
+	 * @return void
+	 * @throws AppException
+	 *             General Exception
+	 */
 	@Override
 	public void createAccount(ConcernRoleKey key) throws AppException,
 			InformationalException {
+		// Call the helper method to create new account
 		MOLSACitizenPortalHelper citizenPortalHelper = new MOLSACitizenPortalHelper();
 		citizenPortalHelper.createNewAccount(key.concernRoleID);
 	}
-
 }
--- Development/EJBServer/components/MOLSA/source/curam/molsacp/maintainaccount/facade/impl/MOLSAResetUserPassword.java
+++ Development/EJBServer/components/MOLSA/source/curam/molsacp/maintainaccount/facade/impl/MOLSAResetUserPassword.java
@@ -7,10 +7,19 @@ import curam.util.exception.InformationalException;
 
 public class MOLSAResetUserPassword extends
 		curam.molsacp.maintainaccount.facade.base.MOLSAResetUserPassword {
-
+	/**
+	 * This method calls the reset function of the citizen portal helper class.
+	 * 
+	 * @param key
+	 *            UserPasswordDetails
+	 * @return void
+	 * @throws AppException
+	 *             General Exception
+	 */
 	@Override
 	public void resetUserPassword(UserPasswordDetails key) throws AppException,
 			InformationalException {
+		//Call the helper method to reset password
 		MOLSACitizenPortalHelper accountHelperObj = new MOLSACitizenPortalHelper();
 		accountHelperObj.resetPassword(key);
 	}
