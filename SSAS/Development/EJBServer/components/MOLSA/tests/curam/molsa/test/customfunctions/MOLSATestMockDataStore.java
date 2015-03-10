69efe66742c3cf75e3f255b3a978249ab4d60154
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-03-10 08:27:52 +0000

    Code coverage-Rajesh

--- SSAS/Development/EJBServer/components/MOLSA/tests/curam/molsa/test/customfunctions/MOLSATestMockDataStore.java
+++ SSAS/Development/EJBServer/components/MOLSA/tests/curam/molsa/test/customfunctions/MOLSATestMockDataStore.java
@@ -3,10 +3,6 @@ package curam.molsa.test.customfunctions;
 import com.google.inject.Inject;
 
 import curam.codetable.impl.RELATIONSHIPTYPECODEEntry;
-import curam.core.sl.impl.RegisterProspectPerson;
-import curam.core.struct.AddressFieldDetails;
-import curam.core.struct.OtherAddressData;
-import curam.core.struct.PersonRegistrationDetails;
 import curam.datastore.impl.Datastore;
 import curam.datastore.impl.DatastoreFactory;
 import curam.datastore.impl.Entity;
@@ -15,24 +11,23 @@ import curam.datastore.impl.NoSuchSchemaException;
 import curam.ieg.impl.IEGConstants;
 import curam.molsa.constants.impl.MOLSADatastoreConst;
 import curam.molsa.test.framework.CuramServerTest;
-import curam.participant.impl.ConcernRole;
 import curam.participant.impl.ConcernRoleDAO;
 import curam.util.exception.AppException;
 import curam.util.exception.AppRuntimeException;
 import curam.util.exception.InformationalException;
-import curam.util.transaction.DatabaseConnection;
 import curam.util.type.Date;
 
-public class MOLSATestMockDataStore extends CuramServerTest{
+public class MOLSATestMockDataStore extends CuramServerTest {
 	public MOLSATestMockDataStore(String arg0) {
 		super(arg0);
 		// TODO Auto-generated constructor stub
 	}
-	 /**
-	   * Data access object for the concern role entity.
-	   */
-	  @Inject
-	  private ConcernRoleDAO concernRoleDAO;
+
+	/**
+	 * Data access object for the concern role entity.
+	 */
+	@Inject
+	private ConcernRoleDAO concernRoleDAO;
 	private static String kDatastoreRootEntityName = "Application";
 	final Entity dataStoreRootEntity = null;
 
@@ -74,12 +69,12 @@ public class MOLSATestMockDataStore extends CuramServerTest{
 	 * 
 	 * @param datastore
 	 * @return rootEntity
-	 * @throws InformationalException 
-	 * @throws AppException 
+	 * @throws InformationalException
+	 * @throws AppException
 	 */
 	public Entity createMockDataStore(final Datastore datastore) {
-		
-		//Create Concern Role
+
+		// Create Concern Role
 		final Entity rootEntity = datastore.newEntity(datastore
 				.getEntityType(kDatastoreRootEntityName));
 		rootEntity.setTypedAttribute("submitDate", Date.getCurrentDate());
@@ -97,13 +92,13 @@ public class MOLSATestMockDataStore extends CuramServerTest{
 		rootEntity.addChildEntity(intakeApplicaton);
 
 		// Add IntakeProgram
-//		final EntityType intakeProgramType = datastore
-//				.getEntityType(MOLSADatastoreConst.kIntakeProgramType);
-//		final Entity intakeProgram = datastore.newEntity(intakeProgramType);
-//		intakeProgram.setTypedAttribute("programTypeID", "MPT17001");
-//		intakeProgram.setTypedAttribute("programTypeReference",
-//				"Social Assistance");
-//		rootEntity.addChildEntity(intakeProgram);
+		// final EntityType intakeProgramType = datastore
+		// .getEntityType(MOLSADatastoreConst.kIntakeProgramType);
+		// final Entity intakeProgram = datastore.newEntity(intakeProgramType);
+		// intakeProgram.setTypedAttribute("programTypeID", "MPT17001");
+		// intakeProgram.setTypedAttribute("programTypeReference",
+		// "Social Assistance");
+		// rootEntity.addChildEntity(intakeProgram);
 
 		// Create Person
 		final EntityType personEntityType = datastore
@@ -173,6 +168,21 @@ public class MOLSATestMockDataStore extends CuramServerTest{
 		address.setTypedAttribute("zone", "MZ17078");
 		personEntity.addChildEntity(address);
 
+		final EntityType personMailingAddressEntityType = datastore
+				.getEntityType(MOLSADatastoreConst.kMailingAddress);
+		final Entity mailingAddress = datastore.newEntity(personMailingAddressEntityType);
+		mailingAddress.setTypedAttribute("mailingAddressID", "264");
+		mailingAddress.setTypedAttribute("mailingBuildingNumber", "123456");
+		mailingAddress.setTypedAttribute("mailingBuildingType", "MBT17001");
+		mailingAddress.setTypedAttribute("mailingCountry", "QA");
+		mailingAddress.setTypedAttribute("mailingElectricityNumber", "1234");
+		mailingAddress.setTypedAttribute("mailingMunicipality", "MM17004");
+		mailingAddress.setTypedAttribute("mailingPOBox", "1234567");
+		mailingAddress.setTypedAttribute("mailingPostOfficeCode", "12335");
+		mailingAddress.setTypedAttribute("mailingStreet", "MS17002");
+		mailingAddress.setTypedAttribute("mailingZone", "MZ17077");
+		personEntity.addChildEntity(mailingAddress);
+
 		final EntityType intakeAppType = datastore
 				.getEntityType(MOLSADatastoreConst.kIntakeApplication);
 
@@ -181,11 +191,12 @@ public class MOLSATestMockDataStore extends CuramServerTest{
 		intakeApp.setTypedAttribute("interpreterRequired", Boolean.FALSE);
 		intakeApp.setTypedAttribute("receivedMethod", "AM19004");
 		rootEntity.addChildEntity(intakeApp);
-		
+
 		// Create Person
 		final EntityType secondPersonEntityType = datastore
 				.getEntityType(IEGConstants.kPersonEntityName);
-		final Entity secondPersonEntity = datastore.newEntity(secondPersonEntityType);
+		final Entity secondPersonEntity = datastore
+				.newEntity(secondPersonEntityType);
 
 		secondPersonEntity.setTypedAttribute("aliasFirstName", "");
 		secondPersonEntity.setTypedAttribute("aliasLastName", "");
@@ -199,11 +210,14 @@ public class MOLSATestMockDataStore extends CuramServerTest{
 		secondPersonEntity.setTypedAttribute("hasHomeRental", Boolean.TRUE);
 		secondPersonEntity.setTypedAttribute("hasIncome", Boolean.TRUE);
 		secondPersonEntity.setTypedAttribute("isChild", Boolean.FALSE);
-		secondPersonEntity.setTypedAttribute("isMailingAddressSame", Boolean.TRUE);
-		secondPersonEntity
-				.setTypedAttribute("isMemberEnrolledInSchool", Boolean.TRUE);
-		secondPersonEntity.setTypedAttribute("isPhysicallyChallenged", Boolean.FALSE);
-		secondPersonEntity.setTypedAttribute("isPrimaryParticipant", Boolean.FALSE);
+		secondPersonEntity.setTypedAttribute("isMailingAddressSame",
+				Boolean.TRUE);
+		secondPersonEntity.setTypedAttribute("isMemberEnrolledInSchool",
+				Boolean.TRUE);
+		secondPersonEntity.setTypedAttribute("isPhysicallyChallenged",
+				Boolean.FALSE);
+		secondPersonEntity.setTypedAttribute("isPrimaryParticipant",
+				Boolean.FALSE);
 		secondPersonEntity.setTypedAttribute("isUnfitToWork", Boolean.FALSE);
 		secondPersonEntity.setTypedAttribute("lastName", "Pai");
 		secondPersonEntity.setTypedAttribute("maritalStatus", "MS1");
@@ -218,32 +232,35 @@ public class MOLSATestMockDataStore extends CuramServerTest{
 
 		final EntityType secondPersonIncomeEntityType = datastore
 				.getEntityType(MOLSADatastoreConst.kIncome);
-		final Entity secondPersonIncome = datastore.newEntity(secondPersonIncomeEntityType);
+		final Entity secondPersonIncome = datastore
+				.newEntity(secondPersonIncomeEntityType);
 		secondPersonIncome.setTypedAttribute("amount", "10000");
 		secondPersonIncome.setTypedAttribute("frequency", "FC3");
 		secondPersonIncome.setTypedAttribute("incomeType", "IT17002");
-		secondPersonIncome.setTypedAttribute("startDate",
-				Date.getCurrentDate().addDays(-100));
+		secondPersonIncome.setTypedAttribute("startDate", Date.getCurrentDate()
+				.addDays(-100));
 		secondPersonEntity.addChildEntity(secondPersonIncome);
 
-		final EntityType expenseSecondPersonExpenseType = datastore.getEntityType("Expense");
-		final Entity expenseSecondPerson = datastore.newEntity(expenseSecondPersonExpenseType);
+		final EntityType expenseSecondPersonExpenseType = datastore
+				.getEntityType("Expense");
+		final Entity expenseSecondPerson = datastore
+				.newEntity(expenseSecondPersonExpenseType);
 		expenseSecondPerson.setTypedAttribute("amount", "10000");
 		expenseSecondPerson.setTypedAttribute("frequency", "FC3");
 		expenseSecondPerson.setTypedAttribute("expenseType", "MEX17000");
-		expenseSecondPerson.setTypedAttribute("startDate",
-				Date.getCurrentDate().addDays(-100));
+		expenseSecondPerson.setTypedAttribute("startDate", Date
+				.getCurrentDate().addDays(-100));
 		secondPersonEntity.addChildEntity(expenseSecondPerson);
-		
+
 		final EntityType relationShipType = datastore
-		.getEntityType(MOLSADatastoreConst.kRelationshipEntity);
+				.getEntityType(MOLSADatastoreConst.kRelationshipEntity);
 		final Entity relationShip = datastore.newEntity(relationShipType);
-		relationShip.setTypedAttribute("personID","261");
-		relationShip.setTypedAttribute("relationshipType",RELATIONSHIPTYPECODEEntry.PARENT.getCode());
-		relationShip.setTypedAttribute("startDate",Date.getCurrentDate());
+		relationShip.setTypedAttribute("personID", "261");
+		relationShip.setTypedAttribute("relationshipType",
+				RELATIONSHIPTYPECODEEntry.PARENT.getCode());
+		relationShip.setTypedAttribute("startDate", Date.getCurrentDate());
 		personEntity.addChildEntity(relationShip);
 
-		
 		return rootEntity;
 	}
 
@@ -301,6 +318,5 @@ public class MOLSATestMockDataStore extends CuramServerTest{
 
 		return entity;
 	}
-	
 
 }
