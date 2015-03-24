0386464559501a9907f90778372ae99f7424439c
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-03-24 08:28:10 +0000

    Modelling changes related to citizen workspace landing page - Harisha

---
+++ SSAS/Development/EJBServer/components/MOLSACP/source/curam/molsacp/landingpage/facade/impl/MOLSALandingPage.java
@@ -0,0 +1,16 @@
+package curam.molsacp.landingpage.facade.impl;
+
+import curam.molsacp.landingpage.struct.MOLSALandingPageDetails;
+import curam.util.exception.AppException;
+import curam.util.exception.InformationalException;
+
+public abstract class MOLSALandingPage extends curam.molsacp.landingpage.facade.base.MOLSALandingPage{
+
+	@Override
+	public MOLSALandingPageDetails getLandingPageData() throws AppException,
+			InformationalException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
