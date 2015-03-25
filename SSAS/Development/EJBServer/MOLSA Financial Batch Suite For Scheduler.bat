0fb12e85517788b8544b45865979ddaf58f8cf16
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-03-25 06:41:40 +0000

    Ditto Pachen- For Cognos

--- SSAS/Development/EJBServer/MOLSA Financial Batch Suite For Scheduler.bat
+++ SSAS/Development/EJBServer/MOLSA Financial Batch Suite For Scheduler.bat
@@ -9,3 +9,4 @@ call build runbatch -Dbatch.program=curam.core.intf.DetermineProductDeliveryElig
 call build runbatch -Dbatch.program=curam.core.intf.GenerateInstructionLineItems.processAllFinancialComponentsDue   -Dbatch.username=SYSTEM -Djava.maxmemory=1500m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
 call build runbatch -Dbatch.program=curam.core.intf.GenerateInstruments.processInstructionLineItemsDue   -Dbatch.username=SYSTEM -Djava.maxmemory=1500m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
 call build runbatch -Dbatch.program=curam.molsa.eft.batch.intf.MOLSAGenerateEFTBatch.process -Dbatch.username=SYSTEM -Djava.maxmemory=1500m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
+call build runbatch -Dbatch.program=curam.molsa.cognos.reporting.intf.MOLSACognosReportHelperBatch.process -Dbatch.username=SYSTEM -Djava.maxmemory=1500m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
