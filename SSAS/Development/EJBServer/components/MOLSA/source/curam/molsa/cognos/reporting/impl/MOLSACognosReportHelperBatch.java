689d0e0cad6765938da4d121216535dfe8ecb061
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-03-25 06:44:08 +0000

    Ditto Pachen - For Cognos

--- SSAS/Development/EJBServer/components/MOLSA/source/curam/molsa/cognos/reporting/impl/MOLSACognosReportHelperBatch.java
+++ SSAS/Development/EJBServer/components/MOLSA/source/curam/molsa/cognos/reporting/impl/MOLSACognosReportHelperBatch.java
@@ -9,7 +9,16 @@ import curam.util.exception.InformationalException;
 import curam.util.resources.Trace;
 import curam.util.transaction.TransactionInfo;
 
+/**
+ * THIS BATCH IS NOT USED FOR CURAM
+ * This is used to populate the payment details into the Cognos Staging Table using a view.
+ *
+ */
 public class MOLSACognosReportHelperBatch extends curam.molsa.cognos.reporting.base.MOLSACognosReportHelperBatch{
+  
+  /**
+   * The batch api to populate the details into the Cognos staging table.
+   */
   @Override
   public void process() throws AppException, InformationalException {
     String sql = " INSERT INTO MOLSACognosPaymentDetails SELECT * FROM VW_ MOLSAPAYMENTDETAILS" ; 
