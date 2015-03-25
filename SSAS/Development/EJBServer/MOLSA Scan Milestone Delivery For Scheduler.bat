4f8e418218d14aeec4d0d0e1c284441f375de90d
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-03-25 07:16:10 +0000

    Ditto Pachen- For Scheduler

---
+++ SSAS/Development/EJBServer/MOLSA Scan Milestone Delivery For Scheduler.bat
@@ -0,0 +1,8 @@
+E:
+CD E:\IBM\Curam\Runtime
+call SetEnvironment.bat
+
+set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_45
+set path=%JAVA_HOME%\bin;%path%
+
+call build runbatch -Dbatch.program=curam.core.sl.intf.ScanMilestoneDeliveryEndDate.processExceededMilestoneEndDate -Dbatch.username=SYSTEM -Djava.maxmemory=1024m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
