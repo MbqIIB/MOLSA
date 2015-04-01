cea53dd30b2554af72e5788fdf3f40ccc75aa035
Diona_Molsa <Diona_Molsa@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-04-01 17:38:09 +0000

    .bat file for MOLSA User Account SMS Batch - Ramesh

---
+++ SSAS/Development/EJBServer/MOLSA User Account SMS Batch.bat
@@ -0,0 +1,1 @@
+call build runbatch -Dbatch.program=curam.molsa.useraccount.sms.sl.intf.MOLSAUserAccountSMSBatchJob.process -Dbatch.username=SYSTEM -Djava.maxmemory=1024m -Djava.extra.jvmargs="-XX:MaxPermSize=512M"
