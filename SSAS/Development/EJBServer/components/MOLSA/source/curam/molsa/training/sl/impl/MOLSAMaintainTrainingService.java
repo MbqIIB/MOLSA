e15c9e4ee6168a6aa041a27c22054bad37e183e4
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-04-10 09:39:13 +0000

    Ditto Pachen - CPM Changes

---
+++ SSAS/Development/EJBServer/components/MOLSA/source/curam/molsa/training/sl/impl/MOLSAMaintainTrainingService.java
@@ -0,0 +1,16 @@
+package curam.molsa.training.sl.impl;
+
+import curam.core.struct.InformationalMsgDtlsList;
+import curam.molsa.training.struct.MOLSATrainingDetails;
+import curam.util.exception.AppException;
+import curam.util.exception.InformationalException;
+
+public class MOLSAMaintainTrainingService extends  curam.molsa.training.sl.base.MOLSAMaintainTrainingService {
+
+  @Override
+  public InformationalMsgDtlsList createBeneficiaryService(MOLSATrainingDetails trainingDetails) throws AppException, InformationalException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
