4422c543e7b1ca9555f1b68f6c1d164367794107
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-06-02 08:32:56 +0000

    Ditto Pachen - Merge on Jun 02

--- SSAS/Development/EJBServer/components/MOLSA/source/curam/molsa/creoleprogramrecommendation/sl/event/impl/MOLSAApplicationDenialHandler.java
+++ SSAS/Development/EJBServer/components/MOLSA/source/curam/molsa/creoleprogramrecommendation/sl/event/impl/MOLSAApplicationDenialHandler.java
@@ -6,16 +6,10 @@ import java.util.List;
 import com.google.inject.Inject;
 
 import curam.application.codetable.impl.APPLICATIONSTATUSEntry;
-import curam.application.entity.struct.ApplicationDtls;
 import curam.application.entity.struct.ApplicationKey;
 import curam.application.impl.Application;
 import curam.application.impl.ApplicationDAO;
-import curam.application.impl.ApplicationStatus;
-import curam.application.impl.ApplicationStatusDAO;
-import curam.application.impl.ProgramApplication;
-import curam.application.impl.ProgramApplicationStatusDAO;
 import curam.application.workflow.struct.ApplicationWorkflowDetails;
-import curam.codetable.PRODUCTTYPE;
 import curam.codetable.impl.MILESTONESTATUSCODEEntry;
 import curam.core.sl.entity.struct.MilestoneDeliveryKey;
 import curam.core.sl.fact.MilestoneDeliveryFactory;
@@ -23,31 +17,19 @@ import curam.core.sl.intf.MilestoneDelivery;
 import curam.core.sl.struct.MilestoneDeliveryDtls;
 import curam.core.sl.struct.ReadMilestoneDeliveryDetails;
 import curam.core.sl.struct.TaskCreateDetails;
-import curam.creoleprogramrecommendation.codetable.impl.SIMULATEDDETERMINATIONDECLINEDREASONEntry;
-import curam.creoleprogramrecommendation.facade.fact.CREOLEProgramRecommendationFactory;
-import curam.creoleprogramrecommendation.facade.intf.CREOLEProgramRecommendation;
-import curam.creoleprogramrecommendation.facade.struct.SimulatedDeterminationDetails;
-import curam.creoleprogramrecommendation.facade.struct.SimulatedDeterminationDetailsList;
-import curam.creoleprogramrecommendation.impl.CREOLEProgramRecommendationDAO;
-import curam.creoleprogramrecommendation.impl.SimulatedDetermination;
-import curam.creoleprogramrecommendation.impl.SimulatedDeterminationManager;
-import curam.events.ApplicationDisposed;
-import curam.message.BPOPRODUCTDELIVERYAPPROVAL;
+import curam.events.MOLSAApplicationNotification;
 import curam.message.MOLSANOTIFICATION;
 import curam.molsa.constants.impl.MOLSAConstants;
 import curam.piwrapper.caseheader.impl.CaseHeader;
 import curam.piwrapper.caseheader.impl.CaseHeaderDAO;
-import curam.piwrapper.user.impl.User;
-import curam.piwrapper.user.impl.UserDAO;
+import curam.util.events.impl.EventFilter;
 import curam.util.events.impl.EventHandler;
-import curam.util.events.impl.EventService;
 import curam.util.events.struct.Event;
 import curam.util.exception.AppException;
 import curam.util.exception.InformationalException;
 import curam.util.exception.LocalisableString;
 import curam.util.persistence.GuiceWrapper;
 import curam.util.transaction.TransactionInfo;
-import curam.util.type.CodeTable;
 import curam.util.type.Date;
 import curam.util.type.DeepCloneable;
 import curam.util.workflow.impl.EnactmentService;
@@ -66,11 +48,18 @@ public class MOLSAApplicationDenialHandler implements EventHandler {
 	public MOLSAApplicationDenialHandler() {
 		GuiceWrapper.getInjector().injectMembers(this);
 	}
-
+	
 	@Override
 	public void eventRaised(Event event) throws AppException,
 			InformationalException {
 
+		if (event.eventKey.eventClass
+				.equals(MOLSAApplicationNotification.APPLICATION_AUTO_DENIAL.eventClass)
+				&& (!event.eventKey.eventType
+						.equals(MOLSAApplicationNotification.APPLICATION_AUTO_DENIAL.eventType))) {
+			return;
+		}
+		
 		MilestoneDelivery deliveryObj = MilestoneDeliveryFactory.newInstance();
 
 		MilestoneDeliveryKey deliveryKey = new MilestoneDeliveryKey();
@@ -146,5 +135,6 @@ public class MOLSAApplicationDenialHandler implements EventHandler {
 
 		milestoneDeliveryObj.modify(modifyMilestoneDeliveryDtls);
 	}
+
 
 }
