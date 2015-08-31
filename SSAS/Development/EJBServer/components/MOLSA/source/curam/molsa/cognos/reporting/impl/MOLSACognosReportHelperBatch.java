66b625045bfdf18ac018d721ef9185bae2e3aeeb
Diona_development <Diona_development@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2015-08-31 13:16:24 +0000

    Ditto Pachen- Cognos Changes

--- SSAS/Development/EJBServer/components/MOLSA/source/curam/molsa/cognos/reporting/impl/MOLSACognosReportHelperBatch.java
+++ SSAS/Development/EJBServer/components/MOLSA/source/curam/molsa/cognos/reporting/impl/MOLSACognosReportHelperBatch.java
@@ -3,6 +3,7 @@ package curam.molsa.cognos.reporting.impl;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import curam.core.sl.struct.BooleanIndicator;
 import curam.molsa.message.MOLSABPOCOGNOSREPORT;
 import curam.util.exception.AppException;
 import curam.util.exception.InformationalException;
@@ -20,11 +21,23 @@ public class MOLSACognosReportHelperBatch extends curam.molsa.cognos.reporting.b
    * The batch api to populate the details into the Cognos staging table.
    */
   @Override
-  public void process() throws AppException, InformationalException {
-    String sql = " INSERT INTO MOLSACognosPaymentDetails SELECT * FROM VW_MOLSAPAYMENTDETAILS" ; 
+  public void process(BooleanIndicator isMidRun) throws AppException, InformationalException {
+    String midRunSql1 = " INSERT INTO MOLSACognosPaymentDetails SELECT * FROM VW_MOLSAPAYMENTDETAILS" ; 
+    String midRunSql2 = " INSERT INTO MOLSACognosPaymentDetails SELECT * FROM VW_MOLSAPAYMENTDETAILS" ; 
+    String normalRunSql1 = " INSERT INTO MOLSACognosPaymentDetails SELECT * FROM VW_MOLSAPAYMENTDETAILS" ; 
+    String normalRunSql2 = " INSERT INTO MOLSACognosPaymentDetails SELECT * FROM VW_MOLSAPAYMENTDETAILS" ; 
     try {
-      PreparedStatement stmt = TransactionInfo.getInfo().getInfoConnection().prepareStatement(sql);
-      stmt.executeQuery();
+      PreparedStatement stmt1 = null;
+      PreparedStatement stmt2 = null;
+      if(isMidRun.flag) {
+    	  stmt1 = TransactionInfo.getInfo().getInfoConnection().prepareStatement(midRunSql1);
+    	  stmt2 = TransactionInfo.getInfo().getInfoConnection().prepareStatement(midRunSql2);
+      } else {
+    	  stmt1 = TransactionInfo.getInfo().getInfoConnection().prepareStatement(normalRunSql1);
+    	  stmt2 = TransactionInfo.getInfo().getInfoConnection().prepareStatement(normalRunSql2);
+      }
+      stmt1.executeQuery();
+      stmt2.executeQuery();
     } catch (SQLException e) {
       Trace.kTopLevelLogger.error(
           "Error Occurred while inseting the records to MOLSACognosPaymentDetails . " + e, e.getCause());
@@ -32,5 +45,7 @@ public class MOLSACognosReportHelperBatch extends curam.molsa.cognos.reporting.b
     }
     
   }
+
+
 
 }
