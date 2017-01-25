42f02834d0e456ea1a02953589efee0fa859373a
gbm_molsa <gbm_molsa@eedea305-3843-2c45-b1c4-a1fe7ee0ae9c>
2017-01-25 10:12:21 +0000

    Joseph - CR 4.3 - Disabled Case Authorize button for CaseWorkers

--- Development/EJBServer/components/MOLSA/source/curam/molsa/creoleprogramrecommendation/facade/impl/MOLSACREOLEProgramRecommendation.java
+++ Development/EJBServer/components/MOLSA/source/curam/molsa/creoleprogramrecommendation/facade/impl/MOLSACREOLEProgramRecommendation.java
@@ -4,15 +4,21 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 import curam.application.entity.struct.ApplicationKey;
+import curam.core.base.Users;
+import curam.core.fact.UsersFactory;
+import curam.core.struct.UsersDtls;
+import curam.core.struct.UsersKey;
 import curam.creoleprogramrecommendation.facade.fact.CREOLEProgramRecommendationFactory;
 import curam.creoleprogramrecommendation.facade.intf.CREOLEProgramRecommendation;
 import curam.creoleprogramrecommendation.facade.struct.SimulatedDeterminationDetails;
 import curam.creoleprogramrecommendation.facade.struct.SimulatedDeterminationDetailsList;
 import curam.creoleprogramrecommendation.struct.CREOLEProgramRecommendationKey;
+import curam.molsa.constants.impl.MOLSAConstants;
 import curam.molsa.creoleprogramrecommendation.facade.struct.MolsaSimulatedDeterminationDetails;
 import curam.molsa.creoleprogramrecommendation.facade.struct.MolsaSimulatedDeterminationDetailsList;
 import curam.util.exception.AppException;
 import curam.util.exception.InformationalException;
+import curam.util.transaction.TransactionInfo;
 import curam.util.type.Date;
 
 /**
@@ -50,11 +56,16 @@ public class MOLSACREOLEProgramRecommendation
 
 		SimulatedDeterminationDtlsList = CreoleProgramRecommendationObj
 				.listEligibleSimulatedDeterminations(key);
+		
+		curam.core.intf.Users usersObj=UsersFactory.newInstance();
+		UsersKey usersKey = new UsersKey();
+		usersKey.userName= TransactionInfo.getProgramUser();
+		UsersDtls usersDtls = usersObj.read(usersKey);
 		for (SimulatedDeterminationDetails dtls : SimulatedDeterminationDtlsList.list) {
 			MolsaSimulatedDeterminationDetails molsaSimulatedDeterminationDtls = new MolsaSimulatedDeterminationDetails();
 			molsaSimulatedDeterminationDtls.dtls.assign(dtls);
 			String date = molsaSimulatedDeterminationDtls.dtls.period
-					.substring(134, 144);
+			.substring(134, 144);
 			//DateFormat finald= new SimpleDateFormat("yyyy-MM-dd");
 			String dates[] = date.split("-");
 			String one = dates[1];
@@ -66,8 +77,12 @@ public class MOLSACREOLEProgramRecommendation
 					|| (molsaSimulatedDeterminationDtls.dtls.isAuthorized)) {
 				molsaSimulatedDeterminationDtls.isPDAuthorized = true;
 			}
+			//Disable the Case Authorize button for Case Workers MOLSA-2017-CR4.3
+			if(usersDtls.roleName.equals(MOLSAConstants.kMolsaCaseWorkerRole)){
+				molsaSimulatedDeterminationDtls.isPDAuthorized = true;
+			}
 			molsaSimulatedDeterminationDtlsList.dtls
-					.addRef(molsaSimulatedDeterminationDtls);
+			.addRef(molsaSimulatedDeterminationDtls);
 
 		}
 		return molsaSimulatedDeterminationDtlsList;
